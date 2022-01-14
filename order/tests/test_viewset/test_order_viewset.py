99143d7c7f60e75fc1b0705a9cae4c4f7ccce0f7
Nicholas Bau <57227204+ncholasb@users.noreply.github.com>
2022-01-13 21:08:04 -0300

    Serializers, tests and updates for route

---
+++ order/tests/test_viewset/test_order_viewset.py
@@ -0,0 +1,55 @@
+from itertools import product
+import json
+
+from rest_framework import status
+from rest_framework import APITestCase, APIClient
+
+from django.urls import reverse
+
+from product.factories import CategoryFactory, ProductFactory
+from order.factories import UserFactory, OrderFactory
+from product.models import Product
+from order.models import Order
+
+
+class TestOrderViewSet(APIClient):
+
+    client = APIClient()
+
+    def setUp(self):
+        self.category = CategoryFactory(title="technology")
+        self.product = ProductFactory(
+            title="mouse", price=100, category=[self.category]
+        )
+        self.order = OrderFactory(product=[self.product])
+
+    def test_order(self):
+        """
+        Test order
+        """
+        response = self.client.get(reverse("order-list", kwargs={"version": "v1"}))
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        order_data = json.loads(response.content)[0]
+        self.assertEqual(order_data["product"][0]["title"], self.product.title)
+        self.assertEqual(order_data["product"][0]["price"], self.product.price)
+        self.assertEqual(order_data["product"][0]["active"], self.product.active)
+        self.assertEqual(
+            order_data["product"][0]["category"][0]["title"], self.product.title
+        )
+
+    def test_create_order(self):
+        user = UserFactory()
+        product = ProductFactory()
+        data = json.dumps({"product_id": [product.id], "user": user.id})
+
+        response = self.client.post(
+            reverse("order-list", kwargs={"version": "v1"}),
+            data=data,
+            content_type="application/json",
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        created_order = Order.objects.get(user=user)
---
+++ product/serializers/product_serializer.py
@@ -0,0 +1,32 @@
+from rest_framework import serializers
+
+from product.models.product import Product
+from product.serializers.category_serializer import CategorySerializer
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    category = CategorySerializer(required=True, many=True)
+    categories_id = serializers.PrimaryKeyRelatedField(
+        queryset=Product.objects.all(), write_only=True, many=True
+    )
+
+    class Meta:
+        model = Product
+        fields = [
+            "id",
+            "title",
+            "description",
+            "price",
+            "active",
+            "category",
+            "categories_id",
+        ]
+
+    def create(self, validated_data):
+        category_data = validated_data.pop("categories_id")
+
+        product = Product.objects.create(**validated_data)
+        for category in category_data:
+            product.categories.add(category)
+
+        return product
