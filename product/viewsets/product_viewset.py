ed5a9a68e7a40fc6be26c8c00ca3c5e87c6244ef
Nicholas Baú <baunbd@gmail.com>
2022-06-12 18:13:24 -0300

    feat: wip

--- product/viewsets/product_viewset.py
+++ product/viewsets/product_viewset.py
@@ -2,20 +2,10 @@ from rest_framework.viewsets import ModelViewSet
 
 from product.models import Product
 from product.serializers.product_serializer import ProductSerializer
-from rest_framework.authentication import (
-    SessionAuthentication,
-    BasicAuthentication,
-    TokenAuthentication,
-)  # authentication
 from rest_framework.permissions import IsAuthenticated  # authentication
 
 
 class ProductViewSet(ModelViewSet):
-    authentication_classes = [
-        SessionAuthentication,
-        BasicAuthentication,
-        TokenAuthentication,
-    ]  # authentication
     permission_classes = [IsAuthenticated]  # authentication
     serializer_class = ProductSerializer
 
